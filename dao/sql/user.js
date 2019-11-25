e08796da0cc928a86e1dc34a9c16e19bf060dce0
wangxuquan <wangxuquan@meituan.com>
2019-11-25 18:50:30 +0800

    1

--- dao/sql/user.js
+++ dao/sql/user.js
@@ -1,6 +1,7 @@
 module.exports = {
     queryByName: 'SELECT * FROM ?? WHERE name = ?',
     queryByAccount: 'SELECT id, name, avatar, password FROM `user` WHERE account = ?',
+    // queryByAccount: 'SELECT id, Concat(name, "：{", account, "}") as co FROM `user` WHERE account = ?', // 计算字段，自定义拼接
     insertUser: 'INSERT INTO ?? SET ?',
     insertUserMulti: 'INSERT INTO user(name, age) VALUES ?'
 };
